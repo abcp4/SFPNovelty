ddf2169bf3b2d468fbad4ed54431bc591f2a02da
Ivan Pereira <navi1921@gmail.com>
2021-04-18 16:19:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,7 @@ import maze
 import player
 env = maze.Maze(1)
 env.render()
-def steps():
-    while(1):
-        #act = random.randint(0,3)
-        #act = player.rollout_act(env)
-        #act =player.mcts_act(env,100,10,do_ns=True)[0]
-        act =player.mo_mcts_act(env,100,10)[0]
-        #act = player.uct_act(env,100,15,discount=0.9,e=0.1)
-        print('possible actions:',env.get_possible_actions())
-        _,r,_=env.step(act)
-        print('reward:',r)
-        env.render()
-        time.sleep(1)
+
 def run():
     #Monte Carlo Tree Search
     #acts =player.mcts_act(env,1000,15,do_ns=False)
@@ -52,15 +41,7 @@ def run():
     #acts =player.mcts_act(env,1000,20,do_ns=True)
     #acts =player.mo_mcts_act(env,1000,20)
     #acts =player.pareto_mcts_act(env,500,20)
-    
-    """
 
-    for act in acts:
-        _,r,_=env.step(act)
-        print('reward:',r)
-        env.render()
-        time.sleep(1)
-    """
 run()
 
 
